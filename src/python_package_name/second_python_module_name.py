83804d76c8806337faa22a43a9ad2eafc6ec3dc2
chandravesh chaudhari <chandraveshchaudhari@gmail.com>
2022-12-20 19:32:43 +0530

    hybrid feature selection

--- src/python_package_name/second_python_module_name.py
+++ src/python_package_name/second_python_module_name.py
@@ -17,5 +17,20 @@ def sample_func(word: str):
     return output
 
 
+class HybridFeatureSelection:
+    def __init__(self, data, headers, labels, type_of_algorithm="all"):
+        self.labels = labels
+        self.headers = headers
+        self.data = data
+        self.type_of_algorithm = type_of_algorithm
+        self.selected_features_data = dict()
+
+    def features_placeholder(self, method_name, type_of_selection, feature_selection_result):
+        self.selected_features_data[method_name] = {"type_of_selection": type_of_selection,
+                                                    "feature_selection_result": feature_selection_result}
+
+        return self.selected_features_data
+
+
 if __name__ == '__main__':
     pass
