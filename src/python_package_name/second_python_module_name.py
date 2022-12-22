1829b7b2c3da7f91a2bb506b0f3b7b128616fdc6
chandravesh chaudhari <chandraveshchaudhari@gmail.com>
2022-12-22 21:24:57 +0530

    hybrid feature selection variables_analysis

--- src/python_package_name/second_python_module_name.py
+++ src/python_package_name/second_python_module_name.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def sample_func(word: str):
     """
     this is a sample func for demonstration of numpy docstring and import methods
@@ -18,19 +21,29 @@ def sample_func(word: str):
 
 
 class HybridFeatureSelection:
-    def __init__(self, data, headers, labels, type_of_algorithm="all"):
+    def __init__(self, data=None, headers=None, labels=None, type_of_algorithm="all"):
         self.labels = labels
         self.headers = headers
         self.data = data
         self.type_of_algorithm = type_of_algorithm
-        self.selected_features_data = dict()
+        self.selected_features_data = defaultdict(list)
 
     def features_placeholder(self, method_name, type_of_selection, feature_selection_result):
-        self.selected_features_data[method_name] = {"type_of_selection": type_of_selection,
-                                                    "feature_selection_result": feature_selection_result}
+        self.selected_features_data[method_name].append({"type_of_selection": type_of_selection,
+                                                         "feature_selection_result": feature_selection_result})
 
-        return self.selected_features_data
+    def variables_analysis(self, stored_data=None):
+        if stored_data:
+            data = stored_data
+        else:
+            data = self.selected_features_data
 
-
-if __name__ == '__main__':
-    pass
+        counter_dict = dict()
+        for method_name, feature_selection_list in data.items():
+            for feature_dict in feature_selection_list:
+                for i in feature_dict["feature_selection_result"]:
+                    if i in counter_dict:
+                        counter_dict[i] += 1
+                    else:
+                        counter_dict[i] = 1
+        return counter_dict
