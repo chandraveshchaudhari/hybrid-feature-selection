ede254adfc7edd97978863df312c1f2434ce79c2
chandravesh chaudhari <chandraveshchaudhari@gmail.com>
2023-02-23 15:23:03 +0530

    new distribution version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setuptools.setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.3',  # Required
+    version='0.0.4',  # Required
 
     # version="0.0.14",
     # This is a one-line description or tagline of what your project does. This
@@ -163,7 +163,7 @@ setuptools.setup(
                       'ReliefF==0.1.2',
                       'scipy==1.9.3',
                       'shap==0.41.0',
-                      'sklearn==0.0.post1',
+                      'scikit-learn',
                       'skrebate==0.62',
                       'systematic-reviewpy==0.0.1',
                       'tensorboard==2.11.0',
